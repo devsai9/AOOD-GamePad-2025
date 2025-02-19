1cc05c520d5187704006a28af438c1573c624783
grekand46 <kluo0113@inst.hcpss.org>
2025-02-19 18:50:52 -0500

    Graphics backend update

--- graphics.js
+++ graphics.js
@@ -1,5 +1,6 @@
 const canvas = document.createElement("canvas");
 const ctx = canvas.getContext("2d");
+canvas.textContent = "Your ancient browser doesn't support canvas";
 
 /*
 
@@ -18,42 +19,80 @@ Origin at center of screen
 
 */
 
-const MIN_PIX_COUNT = 256;
+export const CapStyle = Object.freeze({
+    BUTT: "butt",
+    ROUND: "round",
+    SQUARE: "square"
+});
+
+const ScreenProxy = {
+    offsetX: 0,
+    offsetY: 0,
+    scale: 1
+};
+const RATIO = 16 / 9;
 
 function handleResize() {
     const width = window.innerWidth;
     const height = window.innerHeight;
-    const ratio = width / height;
+    
+    canvas.width = width * window.devicePixelRatio;
+    canvas.height = height * window.devicePixelRatio;
 
-    if (width >= height) {
-        canvas.width = MIN_PIX_COUNT * ratio;
-        canvas.height = MIN_PIX_COUNT;
-    }
-    else {
-        canvas.width = MIN_PIX_COUNT;
-        canvas.height = MIN_PIX_COUNT / ratio;
-    }
+    ScreenProxy.offsetX = canvas.width / 2;
+    ScreenProxy.offsetY = canvas.height / 2;
+    ScreenProxy.scale = ((width / height >= RATIO) ? canvas.height : canvas.width / RATIO) * 0.5;
 }
 
 handleResize();
 window.addEventListener("resize", handleResize);
 document.body.appendChild(canvas);
 
+function transformX(x) {
+    return x * ScreenProxy.scale + ScreenProxy.offsetX;
+}
+
+function transformY(y) {
+    return y * ScreenProxy.scale + ScreenProxy.offsetY;
+}
+
+function transformK(k) {
+    return k * ScreenProxy.scale;
+}
 
 // Exports
 
-export function drawImage(image, x, y) {
-    const [dx, dy] = lastModifier();
-    ctx.drawImage(image, x + Math.round(canvas.width / 2) + dx, y + Math.round(canvas.height / 2) + dy);
+/**
+ * @param { ImageBitmap } image 
+ * @param { number } x 
+ * @param { number } y 
+ */
+export function drawImage(image, [x, y]) {
+    ctx.drawImage(
+        image, 
+        transformX(x), 
+        transformY(y), 
+        transformK(image.width),
+        transformK(image.height)
+    );
 }
 
-export function fillRect(x, y, w, h) {
-    const [dx, dy] = lastModifier();
-    ctx.fillRect(x + Math.round(canvas.width / 2) + dx, y + Math.round(canvas.height / 2) + dy, w, h);
+export function fillRect([x, y], w, h) {
+    const s = ScreenProxy.scale;
+    ctx.fillRect(
+        transformX(x), 
+        transformY(y), 
+        transformK(w),
+        transformK(h)
+    );
 }
 
-export function setColor(color) {
+export function setFill(color) {
     ctx.fillStyle = color;
+}
+
+export function setStroke(color) {
+    ctx.strokeStyle = color;
 }
 
 export function clear(color="#000000") {
@@ -66,17 +105,76 @@ export function clear(color="#000000") {
 export function width() { return canvas.width; }
 export function height() { return canvas.height; }
 
-const modifiers = [];
-
-function lastModifier() {
-    const res = modifiers[modifiers.length - 1];
-    if (res === undefined) return [0, 0];
-    return res;
+export function _ctx() {
+    return ctx;
 }
 
-export function pushModifier(x, y) {
-    modifiers.push([x, y]);
+export function ratio() {
+    return RATIO;
 }
-export function restore() {
-    modifiers.pop();
+
+export function maskEdge() {
+    const currentRatio = canvas.width / canvas.height;
+    ctx.fillStyle = "#000000";
+    if (currentRatio > RATIO) {
+        // Too wide
+        const edge = -RATIO * ScreenProxy.scale + ScreenProxy.offsetX;
+        ctx.fillRect(0, 0, edge, canvas.height);
+        ctx.fillRect(canvas.width - edge, 0, edge, canvas.height);
+    }
+    else {
+        // Too tall
+        const edge = -ScreenProxy.scale + ScreenProxy.offsetY;
+        ctx.fillRect(0, 0, canvas.width, edge);
+        ctx.fillRect(0, canvas.height - edge, canvas.width, edge);
+    }
+}
+
+export function beginPath() {
+    ctx.beginPath();
+}
+
+export function closePath() {
+    ctx.closePath();
+}
+
+export function moveTo([x, y]) {
+    ctx.moveTo(
+        transformX(x),
+        transformY(y)
+    );
+}
+
+export function lineTo([x, y]) {
+    ctx.lineTo(
+        transformX(x),
+        transformY(y)
+    );
+}
+
+export function bezierTo([cx1, cy1], [cx2, cy2], [x, y]) {
+    ctx.bezierCurveTo(
+        transformX(cx1),
+        transformY(cy1),
+        transformX(cx2),
+        transformY(cy2),
+        transformX(x),
+        transformY(y)
+    );
+}
+
+export function stroke() {
+    ctx.stroke();
+}
+
+export function fill() {
+    ctx.fill();
+}
+
+export function setLineWidth(w) {
+    ctx.lineWidth = transformK(w);
+}
+
+export function setLineCap(s) {
+    ctx.lineCap = s;
 }
--- main.js
+++ main.js
@@ -1,7 +1,50 @@
 import * as G from "./graphics.js";
 
+/*
+
+- Palette for different biomes -
+[Plains]
+bg: #8de645
+
+[Desert]
+bg: #d9b962
+
+*/
+
 function draw() {
-    G.clear("#ff0000");
+    G.clear("#d9b962");
+
+    G.setLineCap(G.CapStyle.ROUND);
+
+    G.beginPath();
+    G.setLineWidth(0.1);
+    G.moveTo([0, 0]);
+    G.bezierTo([0.5, 0], [0.5, 0.5], [1.0, 0.5]);
+    G.stroke();
+
+    G.beginPath();
+    G._ctx().setLineDash([10, 20]);
+    G.setStroke("#ffff00");
+    G.setLineWidth(0.008);
+    G.moveTo([0, 0]);
+    G.bezierTo([0.5, 0], [0.5, 0.5], [1.0, 0.5]);
+    G.stroke();
+
+    G.setFill("#075577");
+    G.fillRect([-0.1, -0.1], 0.2, 0.2);
+    
+    G.setFill("#33ffdd");
+    G.fillRect([-0.09, -0.09], 0.18, 0.18);
+
+    // const ctx = G._ctx();
+
+    // ctx.lineWidth = 50;
+    // ctx.lineCap = "round";
+    // ctx.moveTo(0, 0);
+    // ctx.bezierCurveTo(500, 0, 500, 500, 1000, 500);
+
+    // ctx.stroke();
+    G.maskEdge();
 }
 
 draw();
